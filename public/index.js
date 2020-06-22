1634881f31929a91d2a3fd97db803c4e3a10ab1f
Justine Tournier <jvo.tournier@gmail.com>
2020-06-22 14:38:02 +0200

    update js

--- public/index.js
+++ public/index.js
@@ -14,4 +14,27 @@ const displayCameras = (cameras) => {
         div.innerHTML = camera.name;
         list.appendChild(div);
     })
+    cameras.map(camera => {
+        const div = document.createElement("div");
+        div.innerHTML = camera.price;
+        list.appendChild(div);
+    })
+    cameras.map(camera => {
+        const div = document.createElement("div");
+        div.innerHTML = camera.description;
+        list.appendChild(div);
+    })
+    cameras.map(camera => {
+        const div = document.createElement("div");
+        div.innerHTML = camera.lenses;
+        list.appendChild(div);
+    })
+    cameras.map(camera => {
+        const div = document.createElement("div");
+        div.innerHTML = imageUrl;
+        list.appendChild(div);
+    })
 }
+
+
+
